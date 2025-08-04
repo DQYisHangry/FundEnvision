1dcc90fdd87dcb1741ad93cf3a9f9b7cb1441314
DQYisHangry <qianyundeng719@gmail.com>
2025-08-03 19:29:12 -0500

    Update mysql_repository.py

--- db/mysql_repository.py
+++ db/mysql_repository.py
@@ -7,7 +7,7 @@ from db.etf_nav import EtfNav
 import mysql.connector
 
 class MysqlRepository(Repository):
-
+#Set up the database connection configuration
     def __init__(self):
         config = {
             'user': 'root',
@@ -16,6 +16,7 @@ class MysqlRepository(Repository):
             'port': 3306,
             'database': 'funds_db'
         }
+        #Connect to the database and store the connection and cursor
         self.connection = mysql.connector.connect(**config)
         self.cursor = self.connection.cursor()
 
@@ -23,7 +24,7 @@ class MysqlRepository(Repository):
         self.cursor.close()
         self.connection.close()
 
-    def list_nav_by_ticker(self, ticker: str) -> list[EtfNav]:
+    def list_nav_by_ticker(self, ticker: str) -> list[EtfNav]:#Convert each row from the result into an EtfNav objec
         sql = "SELECT ticker, fund_name, nav_date, nav FROM etf_nav WHERE ticker = %s ORDER BY nav_date DESC"
         self.cursor.execute(sql, (ticker,))
         result = []
